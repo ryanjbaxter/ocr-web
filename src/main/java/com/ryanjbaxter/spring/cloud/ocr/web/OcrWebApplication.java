867ed7649e474117f03e4c36b75f8826c80bda4f
Ryan Baxter <rbaxter@pivotal.io>
2018-05-05 12:35:28 -0400

    updates for finchley

--- src/main/java/com/ryanjbaxter/spring/cloud/ocr/web/OcrWebApplication.java
+++ src/main/java/com/ryanjbaxter/spring/cloud/ocr/web/OcrWebApplication.java
@@ -2,13 +2,9 @@ package com.ryanjbaxter.spring.cloud.ocr.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 
 @SpringBootApplication
-@EnableDiscoveryClient 
 @EnableZuulProxy
 public class OcrWebApplication {
 
@@ -16,8 +12,4 @@ public class OcrWebApplication {
         SpringApplication.run(OcrWebApplication.class, args);
     }
 
-    @Bean
-    public AlwaysSampler defaultSampler() {
-        return new AlwaysSampler();
-    }
 }
--- src/test/java/com/ryanjbaxter/spring/cloud/ocr/web/OcrWebApplicationTests.java
+++ src/test/java/com/ryanjbaxter/spring/cloud/ocr/web/OcrWebApplicationTests.java
@@ -1,13 +1,10 @@
 package com.ryanjbaxter.spring.cloud.ocr.web;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = OcrWebApplication.class)
+@SpringBootTest(classes = OcrWebApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @WebAppConfiguration
 public class OcrWebApplicationTests {
 
